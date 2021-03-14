764ae6411fb77c280af6ed89e9b6ceaf5ab6ac4f
capricornus44 <antonzavalniuk@gmail.com>
2021-03-14 10:19:39 +0100

    Add modal plugin basicLightbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// Notification plugin PNotify
 import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
-
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
+
+// Modal plugin BasicLightbox
+import * as basicLightbox from 'basiclightbox';
 
 import './styles.css';
