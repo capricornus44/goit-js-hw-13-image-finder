9886f9d4bc5fe38a7848a54b24baa9112b62bf52
capricornus44 <antonzavalniuk@gmail.com>
2021-03-14 10:09:50 +0100

    Add notification plugin PNotify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
 import './styles.css';
