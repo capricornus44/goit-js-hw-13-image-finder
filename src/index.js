043b10c959acb23a17b6b2a126202573278e8474
capricornus44 <antonzavalniuk@gmail.com>
2021-03-15 18:58:31 +0100

    Add script to gallery rendering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,76 @@
-// Notification plugin PNotify
-import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import './styles.css';
+import PixabayApiService from './js/apiService';
+import galleryCardTpl from './template/gallery-card.hbs';
+import { refs } from './js/references';
+
+// Import of notification plugin
+import { alert } from '@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
-// Modal plugin BasicLightbox
-import * as basicLightbox from 'basiclightbox';
+// Import of function to open modal
+import { onOpenModal } from './js/modal';
 
-import './styles.css';
+//========================= <GALLERY RENDERING> ==================================
+const pixabayApiService = new PixabayApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.gallery.addEventListener('click', onOpenModal);
+
+function onSearch(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  pixabayApiService.query = form.elements.query.value;
+
+  if (pixabayApiService.query === '') {
+    alert({
+      text: 'Please enter a word!',
+    });
+    return;
+  }
+
+  if (pixabayApiService.query.trim().length === 0) {
+    alert({
+      text: 'Please enter a more specific query!',
+    });
+    return;
+  }
+
+  pixabayApiService.resetPage();
+  pixabayApiService.fetchArticles().then(data => {
+    clearGallery();
+    appendGalleryMarkup(data);
+    //console.log(data);
+  });
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+function appendGalleryMarkup(data) {
+  refs.gallery.insertAdjacentHTML('beforeend', galleryCardTpl(data));
+}
+//========================= END <GALLERY RENDERING> ==================================
+
+//========================= <INFINITE SCROLL> ==================================
+const options = {
+  rootMargin: '150px',
+  threshold: 0,
+};
+
+const onEntry = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && pixabayApiService.query !== '') {
+      console.log(entry);
+      pixabayApiService.fetchArticles().then(data => {
+        appendGalleryMarkup(data);
+      });
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry, options);
+
+observer.observe(refs.scroll);
+//========================= END <INFINITE SCROLL> ==================================
