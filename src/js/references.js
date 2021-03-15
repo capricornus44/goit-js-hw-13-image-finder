d86d56f80a5e8610968f35360a587e4ce174143c
capricornus44 <antonzavalniuk@gmail.com>
2021-03-15 18:55:03 +0100

    Create references.js

---
+++ src/js/references.js
@@ -0,0 +1,7 @@
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  scroll: document.querySelector('#infinite-scroll'),
+};
+
+export { refs };
